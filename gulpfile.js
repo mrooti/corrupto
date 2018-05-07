55ee1a9d605efcc8f55cb2913a8738407a3cc3a0
Rogelio Fuentes Alvarez <rogelioroger281@gmail.com>
2018-05-06 23:18:17 -0500

    Inicio de proyecto Corrupto
    Sitio web para la visualziación de historieta animada

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+
+gulp.task('watch',['browserSync','sass'], function() {
+  //watch function
+  gulp.watch('app/scss/**/*.scss',['sass']);
+  gulp.watch('app/*.html').on('change', browserSync.reload);
+  gulp.watch('app/js/**/*.js').on('change', browserSync.reload);
+});
+
+//sass compilate
+gulp.task('sass', function(){
+  return gulp.src('app/scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.stream())
+});
+
+//Server
+gulp.task('browserSync', function(){
+  browserSync.init({
+    server:{
+      baseDir:'app'
+    }
+  })
+});
+
+//Optimización de código
+gulp.task('useref', function(){
+  return gulp.src('app/*.html')
+  .pipe(useref())
+  .pipe(gulp.dest(dist))
+});
+
+//uglify
+gulp.task('useref', function(){
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    // Minifies only if it's a JavaScript file
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulp.dest('dist'))
+});
